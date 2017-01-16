69d7f96df9aadb55553a6e346b67919d31fe4243
Connorelsea <connorelsea@gmail.com>
2017-01-15 21:49:20 -0600

    Add ESLint and correct styles accordingly

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "eqeqeq": "error",
+        "curly": "multi",
+        "no-unexpected-multiline": "error",
+        "no-case-declarations": "warn",
+        "no-console": [
+            "warn"
+        ],
+        "indent": [
+            "error",
+            2,
+            { "SwitchCase": 1 }
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
